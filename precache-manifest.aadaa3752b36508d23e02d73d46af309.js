6582fd0a2809bfd8741e62bc670488ed929fe155
patel-tirth <pateltirth1750@gmail.com>
2020-08-17 13:06:15 -0400

    Updates

---
+++ precache-manifest.aadaa3752b36508d23e02d73d46af309.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ccce5ddcb5430a311280bb78e9c7091",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74df2f0fac1c7e50f06f",
+    "url": "/static/css/main.a2f67856.chunk.css"
+  },
+  {
+    "revision": "b0b019fec6a5ac4ae6b5",
+    "url": "/static/js/2.871ed616.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/static/js/2.871ed616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74df2f0fac1c7e50f06f",
+    "url": "/static/js/main.72aa8b70.chunk.js"
+  },
+  {
+    "revision": "db3a36cc780daa9cbff8",
+    "url": "/static/js/runtime-main.b12733e7.js"
+  }
+]);
